70c5b3bfd7209a9ba0ea48ebe8a6cf176b6bd09f
doquocsang <quocsang316@gmail.com>
2024-11-11 00:40:29 +0700

    Hoàn thành logic thêm, xóa item

--- src/src/app/components/todo-item/todo-item.component.ts
+++ src/src/app/components/todo-item/todo-item.component.ts
@@ -1,13 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
 })
 export class TodoItemComponent {
   @Input({ required: true }) TodoItem!: Todo;
+  @Output("deleteFunction") deleteItemEvent = new EventEmitter<string>();
+  @Output("updateFunction") updateItemEvent = new EventEmitter<string>();
+
+  editable: boolean = false;
+
+  DeleteItem() {
+    this.deleteItemEvent.emit(this.TodoItem.id);
+  }
+
+  ToggleEdit() {
+    this.editable = !this.editable;
+  }
+
+  UpdateItem() {
+    this.ToggleEdit();
+    this.updateItemEvent.emit(this.TodoItem.id);
+  }
 }
--- src/src/app/components/todo-panel/todo-panel.component.ts
+++ src/src/app/components/todo-panel/todo-panel.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { v4 as uuidv4 } from 'uuid';
 
 import { Todo } from '../../models/todo';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
@@ -7,27 +10,53 @@ import { TodoItemComponent } from '../todo-item/todo-item.component';
 @Component({
   selector: 'app-todo-panel',
   standalone: true,
-  imports: [TodoItemComponent, CommonModule],
+  imports: [TodoItemComponent, CommonModule, FormsModule],
   templateUrl: './todo-panel.component.html',
 })
 export class TodoPanelComponent {
   TodoList: Todo[] = [];
+  InputTodo: string = "";
 
   ngOnInit(): void {
     for (let i = 0; i < 8; i++) {
       this.TodoList.push({
-        id: 1,
+        id: uuidv4(),
         name: 'This is demo',
-        status: false,
+        status: i%2 === 0,
       });
     }
   }
 
   formatNumber(num: number) {
-    return num > 10 ? num : '0' + num;
+    return num >= 10 ? num : '0' + num;
   }
 
-  removeItem() {}
+  createNewItem() {
+     this.TodoList.push({
+      id: uuidv4(),
+      name: this.InputTodo,
+      status: false,
+   });
+
+   this.InputTodo = "";
+  }
+
+  deleteItem(id: string) {
+    const deleteIndex = this.TodoList.map(item => item.id).indexOf(id);
+    this.TodoList.splice(deleteIndex, 1)
+  }
+
+  updateItem(id: string) {
+   alert("update"+ id)
+  }
+
+  deleteAllCheckedItems() {
+    this.TodoList = this.TodoList.filter(item => !item.status)
+  }
+
+  toggleAllActiveItems() {
+    this.TodoList = this.TodoList.filter(item => !item.status)
+  }
 
   addItem() {}
 }
