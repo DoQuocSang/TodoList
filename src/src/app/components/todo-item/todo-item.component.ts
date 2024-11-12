57f4ab43e161edf389949f87f31b58f29e903c88
DoQuocSang <quocsang316@gmail.com>
2024-11-12 11:39:15 +0700

    Hoàn thành chuyển sang service

--- src/src/app/components/todo-item/todo-item.component.ts
+++ src/src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
-  EventEmitter,
+  inject,
   Input,
   OnChanges,
-  Output,
   SimpleChanges,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -19,11 +19,13 @@ import { Todo } from '../../models/todo';
 })
 export class TodoListComponent implements OnChanges {
   @Input({ required: true }) todoList!: Todo;
-  @Output('deleteFunction') deleteItemEvent = new EventEmitter<string>(); // delete by id
-  @Output('updateFunction') updateItemEvent = new EventEmitter<Todo>(); // update by obj
+  // @Output('deleteFunction') deleteItemEvent = new EventEmitter<string>(); // delete by id
+  // @Output('updateFunction') updateItemEvent = new EventEmitter<Todo>(); // update by obj
 
   editable: boolean = false;
   inputName: string = '';
+
+  todoService: TodoService = inject(TodoService);
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
@@ -33,18 +35,18 @@ export class TodoListComponent implements OnChanges {
     this.inputName = this.todoList.name;
   }
 
-  deleteItem() {
-    this.deleteItemEvent.emit(this.todoList.id);
-  }
-
   toggleEdit() {
     this.editable = !this.editable;
   }
 
-  updateItem() {
+  handleUpdateItem() {
     // console.log((e.target as HTMLParagraphElement).textContent);
     this.toggleEdit();
     this.todoList.name = this.inputName;
-    this.updateItemEvent.emit(this.todoList);
+    this.todoService.updateItem(this.todoList);
+  }
+
+  handleDeleteItem() {
+    this.todoService.deleteItem(this.todoList.id);
   }
 }
