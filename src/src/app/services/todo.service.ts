b4ea715bc0d13dabaad45f2d1fb245814aca968c
DoQuocSang <quocsang316@gmail.com>
2024-11-20 11:01:55 +0700

    Hoàn thành convert sang rxjs

--- src/src/app/services/todo.service.ts
+++ src/src/app/services/todo.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { loremIpsum } from 'lorem-ipsum';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Tab } from '../models/tab';
@@ -15,7 +14,7 @@ type State = {
 
 const initialState = {
   items: [],
-  filterType: Tab.All,
+  filterType: 'all' as Tab,
 };
 
 @Injectable({
@@ -24,7 +23,7 @@ const initialState = {
 export class TodoService {
   todoList: Todo[] = [];
   http: HttpClient = inject(HttpClient);
-  limit: number = 0;
+  limit: number = 10;
 
   private _state: BehaviorSubject<State> = new BehaviorSubject<State>(
     initialState
@@ -32,15 +31,32 @@ export class TodoService {
 
   state$: Observable<State> = this._state.asObservable();
 
-  constructor() {
-    this.limit = 10;
-    for (let i = 0; i < 8; i++) {
-      this._state.value.items.push({
-        id: uuidv4(),
-        title: loremIpsum(),
-        completed: i % 2 === 0,
-      });
-    }
+  filteredItems$: Observable<Todo[]> = this._state.pipe(
+    map(({ items, filterType }) => {
+      switch (filterType) {
+        case 'active':
+          return items.filter((item) => !item.completed);
+        case 'completed':
+          return items.filter((item) => item.completed);
+        default:
+          return items;
+      }
+    })
+  );
+
+  // get filteredItems() {
+  //   switch (this._state.value.filterType) {
+  //     case 'active':
+  //       return this._state.value.items.filter((item) => !item.completed);
+  //     case 'completed':
+  //       return this._state.value.items.filter((item) => item.completed);
+  //     default:
+  //       return this._state.value.items;
+  //   }
+  // }
+
+  setTabFilter(filterType: Tab) {
+    this.patchState({ filterType });
   }
 
   loadData() {
@@ -48,10 +64,12 @@ export class TodoService {
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
         params: { _limit: this.limit },
       })
-      .subscribe((data) => {
-        // console.log(data);
-        this._state.value.items = data;
-      });
+      .pipe(
+        tap((data) => {
+          this.patchState({ items: data });
+        })
+      )
+      .subscribe();
   }
 
   getTodoList(): Todo[] {
@@ -59,53 +77,53 @@ export class TodoService {
   }
 
   isEmptyTodoList(): boolean {
-    return this.todoList.length === 0;
+    return this._state.value.items.length === 0;
   }
 
   getNumberItemsLeft() {
-    return this.todoList.filter((item) => !item.completed).length;
+    return this._state.value.items.filter((item) => !item.completed).length;
   }
 
   getNumberItemsCompleted() {
-    return this.todoList.filter((item) => item.completed).length;
+    return this._state.value.items.filter((item) => item.completed).length;
   }
 
   checkCompletedItems() {
-    return this.todoList.filter((item) => item.completed).length > 0;
+    return this._state.value.items.filter((item) => item.completed).length > 0;
   }
 
-  getTotoListByTab(tab: string) {
-    switch (tab) {
-      case Tab.Completed: {
-        return this.todoList.filter((item) => item.completed);
+  toggleItem(id: string) {
+    const updatedItems = this._state.value.items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      } else {
+        return item;
       }
-      case Tab.Active: {
-        return this.todoList.filter((item) => !item.completed);
-      }
-      case Tab.All: {
-        return this.todoList;
-      }
-      default: {
-        return this.todoList;
-      }
-    }
+    });
+
+    this.patchState({ items: updatedItems });
   }
 
   completeAllItems() {
-    if (
-      this.getNumberItemsCompleted() === 0 ||
-      this.getNumberItemsLeft() === 0
-    ) {
-      this.todoList.forEach((item) => {
-        item.completed = !item.completed;
-      });
-    } else {
-      this.todoList
-        .filter((item) => !item.completed)
-        .forEach((item) => {
-          item.completed = !item.completed;
-        });
-    }
+    const updatedItems = this._state.value.items.map((item) => {
+      if (
+        this.getNumberItemsCompleted() === 0 ||
+        this.getNumberItemsLeft() === 0 ||
+        !item.completed
+      ) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      } else {
+        return item;
+      }
+    });
+
+    this.patchState({ items: updatedItems });
   }
 
   createNewItem(input: string) {
@@ -113,39 +131,44 @@ export class TodoService {
       return;
     }
 
-    // this.todoList.push({
-    //   id: uuidv4(),
-    //   title: input,
-    //   completed: false,
-    // });
-
-    this.http
-      .post<Todo>('https://jsonplaceholder.typicode.com/todos', {
+    const newItems: Todo[] = [
+      {
         id: uuidv4(),
         title: input,
         completed: false,
-      })
-      .subscribe(
-        (data) => {
-          console.log('Create item successfully!', data);
-        },
-        (error) => {
-          console.error('Error create item', error);
-        }
-      );
-  }
+      },
+      ...this._state.value.items,
+    ];
 
-  deleteItem(id: string) {
-    const deleteIndex = this.todoList.map((item) => item.id).indexOf(id);
-    this.todoList.splice(deleteIndex, 1);
+    this.patchState({ items: newItems });
   }
 
   updateItem(updateValue: Todo) {
-    const index = this.todoList.findIndex((item) => item.id === updateValue.id);
-    this.todoList[index] = updateValue;
+    const updatedItems = this._state.value.items.map((item) => {
+      return item.id === updateValue.id ? (item = updateValue) : item;
+    });
+
+    this.patchState({ items: updatedItems });
+  }
+
+  deleteItem(id: string) {
+    const updatedItems = this._state.value.items.filter(
+      (item) => item.id !== id
+    );
+
+    this.patchState({ items: updatedItems });
   }
 
   deleteAllCheckedItems() {
-    this.todoList = this.todoList.filter((item) => !item.completed);
+    const newItems = this._state.value.items.filter((item) => !item.completed);
+    this.patchState({ items: newItems });
+  }
+
+  private patchState(next: Partial<State>) {
+    this._state.next({ ...this._state.value, ...next });
+
+    this._state.subscribe((state) => {
+      console.log('State updated:', state.items);
+    });
   }
 }
